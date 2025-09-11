96ce7188a389254220d0a8e5643f9ddc7a352992
kongiok <ong.kongiok@gmail.com>
2025-09-12 03:40:26 +0800

    feat(syntax): add hello world project

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -3,4 +3,13 @@ const zig = @import("zig");
 
 pub fn main() void {
     std.debug.print("Hello {s}!\n", .{"Giok"});
+    const x: i32 = 2;
+    const y: i32 = 0;
+    const result = zig.add_two_numbers(x, y) catch |err| {
+        std.debug.print("error: invalid Number with {}\n", .{err});
+        std.process.exit(1);
+        unreachable;
+    };
+    std.debug.print("result: {d}\n", .{result});
+    return;
 }
--- zig/src/root.zig
+++ zig/src/root.zig
@@ -1,1 +1,25 @@
 const std = @import("std");
+
+pub const ValueError = error{
+    Invalid,
+};
+
+pub fn add_two_numbers(
+    x: i32,
+    y: i32,
+) !i32 {
+    if (0 > x or 0 > y) {
+        return ValueError.Invalid;
+    }
+    return y + x;
+}
+
+test "args should greater then 0" {
+    try std.testing.expectError(ValueError.Invalid, add_two_numbers(-1, 0));
+    try std.testing.expectError(ValueError.Invalid, add_two_numbers(0, -1));
+}
+
+test "function should be equalevant" {
+    try std.testing.expectEqual(3, add_two_numbers(1, 2));
+    try std.testing.expectEqual(5, add_two_numbers(0, 5));
+}
